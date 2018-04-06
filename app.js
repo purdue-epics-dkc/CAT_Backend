36f2a692828cdb717d784bd4155fe4063e8cf7f5
Joe Mynhier <jwmynhie@imail.iu.edu>
2018-04-05 22:06:52 -0400

    Added Azure Functions configuration.

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const standin = require('./build/Release/standin');
 
 // For now call it each time a new frame is pushed to the queue.
 // It may be more efficient to wait for a chunk of frames before starting.
-module.exports = function(context, dataTrigger, oldState, count) {
-  var standinInput = {oldState: oldState, count: count, data: dataTrigger};
+module.exports = function(context, data, oldState, count) {
+  if (count == null) {
+    count = 0;
+  }
+  if (oldState == null) {
+    oldState = 11;
+  }
+
+  var standinInput = {oldState: oldState, count: count, data: data};
   var standinOutput = standin.Parse(standinInput)
 
   // Preserve state information for next call.
